1449bfebbcb44708e888c3d4d881373c6be30930
jml <jml@WINNER>
2024-06-24 14:27:54 +0200

    ok.

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # SignYourApk
 
+[logo](logo.svg)
+
 ## apksigner tool Android App
+
+[Sign it](Screenshot.png)
+
+
+Original source code :
+
+https://deb.debian.org/debian/pool/main/a/android-platform-tools-apksig/android-platform-tools-apksig_31.0.2.orig.tar.gz
 
 
 https://www.bouncycastle.org/download/bouncy-castle-java/
@@ -33,5 +42,5 @@ apksigner verify app.apk
 
 
 -----------------
-"C:\Program Files\Android\Android Studio\jbr\bin\keytool.exe"  -genkey -keyalg RSA -alias cert -keystore cert.bks -storepass 12345678 -validity 360 -storetype BKS  -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath C:\Users\jml\Downloads\bcprov-jdk18on-1.78.1.jar
+"C:\Program Files\Android\Android Studio\jbr\bin\keytool.exe"  -genkey -keyalg RSA -alias cert -keystore cert.bks -storepass 12345678 -validity 36000 -storetype BKS  -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath C:\Users\jml\Downloads\bcprov-jdk18on-1.78.1.jar
 ----------------
--- app/src/main/java/com/cod5/signyourapk/MainActivity.java
+++ app/src/main/java/com/cod5/signyourapk/MainActivity.java
@@ -1,27 +1,43 @@
 package com.cod5.signyourapk;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.apksigner.ApkSignerTool;
 import com.cod5.signyourapk.databinding.ActivityMainBinding;
 
+import org.conscrypt.BuildConfig;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int STORAGE_PERMISSION_CODE = 189;
 
     // Used to load the 'signyourapk' library on application startup.
     static {
@@ -36,21 +52,95 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        Intent intent = new Intent()
-                .setType("*/*")
-                .setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select an APK file"), 123);
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+
+        /*TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());*/
+        binding.sampleText.setText("Sign you APK");
         binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onClickMe(v);
             }
         });
+        hasWriteStoragePermission();
+        askAllFilesPermission();
+        listDownloadsFiles();
+    }
+
+            private void listDownloadsFiles()
+    {
+        File d = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File[] lst = d.listFiles();
+        int i  = 0;
+        for(File f: lst) {
+            if (f.getName().endsWith(".apk")) {
+                RadioButton r;
+                r = new RadioButton(this);
+                r.setText(f.getAbsolutePath());
+                binding.radio.addView(r, i);
+                i++;
+            }
+        }
+    }
+    /* print result of permission request */
+    public void onRequestPermissionsResult(int requestCode,
+                                            String[] permissions,
+                                            int [] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Storage Permission Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Storage Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private boolean askAllFilesPermission() {
+        if (Build.VERSION.SDK_INT >= 30) {
+            if (hasAllFilesPermission()) {
+                return true;
+            }
+            startActivity(
+                    new Intent(
+                            Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION,
+                            Uri.parse("package:" + getPackageName())
+                    )
+            );
+        }
+        return true;
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    private boolean hasAllFilesPermission()  {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            return Environment.isExternalStorageManager();
+        }
+        return true;
+    }
+
+    private boolean hasWriteStoragePermission()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            return true;
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                    PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        STORAGE_PERMISSION_CODE
+                );
+                return false;
+            }
+        }
+        return true;
+
     }
     public void onClickMe(View v) {
+        binding.sampleText.setText("Start...");
+        Toast.makeText(MainActivity.this, "Start signing", Toast.LENGTH_LONG).show();
+
         try {
             InputStream in = null;
             OutputStream out = null;
@@ -58,14 +148,19 @@ public class MainActivity extends AppCompatActivity {
             in = assetManager.open("cert.bks");
             out = new FileOutputStream(this.getApplicationInfo().dataDir.toString() + "/cert.bks");
             copyFile(in, out);
-
-            ApkSignerTool.main(new String[]{"sign", "--min-sdk-version", "16", "--ks",
-                    this.getApplicationInfo().dataDir.toString() + "/cert.bks",
-                    "--ks-pass", "pass:12345678", binding.sampleText.getText().toString()});
+            int id = binding.radio.getCheckedRadioButtonId();
+            if (id >= 0) {
+                RadioButton rdb = binding.radio.findViewById(id);
+                ApkSignerTool.main(new String[]{"sign", "--min-sdk-version", "16", "--ks",
+                        this.getApplicationInfo().dataDir.toString() + "/cert.bks",
+                        "--ks-pass", "pass:12345678", rdb.getText().toString()});
+                binding.sampleText.setText("APK signed !");
+                Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_LONG).show();
+            }
         } catch (Exception e) {
             e.printStackTrace();
+            binding.sampleText.setText(e.getCause().toString());
         }
-        Toast.makeText(this, "hello", Toast.LENGTH_LONG);
     }
     // Method used by copyAssets() on purpose to copy a file.
     private void copyFile(InputStream in, OutputStream out) throws IOException {
